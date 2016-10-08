283ec4c057515d6b2957195096b5be18594468d6
paul katsen <pkpp1233@gmail.com>
2016-10-08 15:42:19 -0700

    Add pathfinding and obstacle avoidance

---
+++ InitialProj/Assets/SampleAgentScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleAgentScript : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start()
+
+    {
+
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+        for (int index = 0; index < waypoints.Length; index++)
+        {
+            if (waypoints[index].GetComponent<FlowManagerProps>().IsCurrentWayPoint)
+            {
+                agent.SetDestination(waypoints[index].transform.position);
+            }
+        }
+    }
+}
